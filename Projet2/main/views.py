f7cf3c62746ba5f75ac2281b7704c39b22eb66b4
Punkachuros <dylan2503@hotmail.com>
2024-10-16 11:28:22 +0200

    Création projet Django

---
+++ Projet2/main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Article
+
+def home(request):
+    articles = Article.objects.all()
+    return render(request, 'home.html', {'articles': articles})
